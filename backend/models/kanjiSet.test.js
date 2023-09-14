0a6210c91e03386c9467a23816347a5cb9f24687
andy9299 <andy.tu998@gmail.com>
2023-09-14 01:16:33 -0400

    uncommented some tests

--- backend/models/kanjiSet.test.js
+++ backend/models/kanjiSet.test.js
@@ -16,89 +16,89 @@ beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
 
-// describe("getById", function () {
-//   test("works", async function () {
-//     let res = await KanjiSet.getById(testSetIds[1]);
-//     expect(res).toEqual({
-//       username: "testuser2",
-//       name: "set2",
-//       characters: ["協", "刊"]
-//     });
-//   });
-//   test("Not found error on invalid numerical id", async function () {
-//     try {
-//       await KanjiSet.getById(0);
-//       fail();
-//     } catch (err) {
-//       expect(err instanceof NotFoundError).toBeTruthy();
-//     }
-//   });
-// });
+describe("getById", function () {
+  test("works", async function () {
+    let res = await KanjiSet.getById(testSetIds[1]);
+    expect(res).toEqual({
+      username: "testuser2",
+      name: "set2",
+      characters: ["協", "刊"]
+    });
+  });
+  test("Not found error on invalid numerical id", async function () {
+    try {
+      await KanjiSet.getById(0);
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
 
-// describe("getByUsername", function () {
-//   test("works", async function () {
-//     let res = await KanjiSet.getByUsername("testuser2");
-//     expect(res).toEqual([
-//       {
-//         characters: ["撤"],
-//         id: testSetIds[0],
-//         name: "set1"
-//       },
-//       {
-//         characters: ["協", "刊"],
-//         id: testSetIds[1],
-//         name: "set2"
-//       }
-//     ]);
-//   });
-//   test("works on user with no sets", async function () {
-//     let res = await KanjiSet.getByUsername("testuser1");
-//     expect(res).toEqual([]);
-//   });
-//   test("Not found error on invalid username", async function () {
-//     try {
-//       await KanjiSet.getByUsername("invalid");
-//       fail();
-//     } catch (err) {
-//       expect(err instanceof NotFoundError).toBeTruthy();
-//     }
-//   });
-// });
+describe("getByUsername", function () {
+  test("works", async function () {
+    let res = await KanjiSet.getByUsername("testuser2");
+    expect(res).toEqual([
+      {
+        characters: ["撤"],
+        id: testSetIds[0],
+        name: "set1"
+      },
+      {
+        characters: ["協", "刊"],
+        id: testSetIds[1],
+        name: "set2"
+      }
+    ]);
+  });
+  test("works on user with no sets", async function () {
+    let res = await KanjiSet.getByUsername("testuser1");
+    expect(res).toEqual([]);
+  });
+  test("Not found error on invalid username", async function () {
+    try {
+      await KanjiSet.getByUsername("invalid");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
 
-// describe("createKanjiSet", function () {
-//   test("works", async function () {
-//     let res = await KanjiSet.createKanjiSet("testuser1", "testset");
-//     expect(res).toEqual({
-//       username: "testuser1",
-//       name: "testset",
-//       id: testSetIds[1] + 1
-//     });
-//     let setCount = await db.query(
-//       "SELECT * FROM kanji_sets WHERE username = $1"
-//       , ["testuser1"]);
-//     expect(setCount.rows.length).toEqual(1);
-//   });
+describe("createKanjiSet", function () {
+  test("works", async function () {
+    let res = await KanjiSet.createKanjiSet("testuser1", "testset");
+    expect(res).toEqual({
+      username: "testuser1",
+      name: "testset",
+      id: testSetIds[1] + 1
+    });
+    let setCount = await db.query(
+      "SELECT * FROM kanji_sets WHERE username = $1"
+      , ["testuser1"]);
+    expect(setCount.rows.length).toEqual(1);
+  });
 
-//   test("Not found error on invalid username", async function () {
-//     try {
-//       await KanjiSet.createKanjiSet("invalid", "testset");
-//       fail();
-//     } catch (err) {
-//       expect(err instanceof NotFoundError).toBeTruthy();
-//     }
-//   });
+  test("Not found error on invalid username", async function () {
+    try {
+      await KanjiSet.createKanjiSet("invalid", "testset");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
 
-//   test("BadRequestError if hitting set limit", async function () {
-//     try {
-//       for (let i = 0; i < 21; i++) {
-//         await KanjiSet.createKanjiSet("testuser1", `testset${i}`);
-//       }
-//       fail();
-//     } catch (err) {
-//       expect(err instanceof BadRequestError).toBeTruthy();
-//     }
-//   });
-// });
+  test("BadRequestError if hitting set limit", async function () {
+    try {
+      for (let i = 0; i < 21; i++) {
+        await KanjiSet.createKanjiSet("testuser1", `testset${i}`);
+      }
+      fail();
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });
+});
 
 describe("insertChar", function () {
   test("works", async function () {
